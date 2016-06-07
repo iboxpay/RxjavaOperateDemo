17bb4176c616cfb24edc8d5679ab1ecabc24f179
conan <conan1160113614@126.com>
2016-06-07 20:56:10 +0800

    1：首次提交默认代码

---
+++ app/src/main/java/zhangwenhao/kuafu/com/kuafu_retrofit_exercise/MainActivity.java
@@ -0,0 +1,13 @@
+package zhangwenhao.kuafu.com.kuafu_retrofit_exercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
