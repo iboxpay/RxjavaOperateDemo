faadef98294696a28a47000370280d803089b5c8
conan <conan1160113614@126.com>
2016-06-07 21:30:44 +0800

    1:添加依赖 了解RX 最基本的使用

--- app/src/main/java/zhangwenhao/kuafu/com/kuafu_retrofit_exercise/MainActivity.java
+++ app/src/main/java/zhangwenhao/kuafu/com/kuafu_retrofit_exercise/MainActivity.java
@@ -1,13 +1,73 @@
 package zhangwenhao.kuafu.com.kuafu_retrofit_exercise;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testFuncation(0);//RxJava基础概念的练习
+    }
+
+    private void testFuncation(int i) {
+
+        switch (i) {
+            case 0:
+                method0();
+                break;
+        }
+
+
+    }
+
+    /**
+     *
+     */
+    private void method0() {
+        //创建observable
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello");
+                subscriber.onNext("RxJava");
+                subscriber.onNext("!");
+                subscriber.onCompleted();
+            }
+        });
+
+        //创建observer
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted: ");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onNext(String s) {
+
+                Log.d(TAG, "onNext: " + s);
+            }
+        };
+
+        //订阅
+        observable.subscribe(observer);
+
     }
 }
