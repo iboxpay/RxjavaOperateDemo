66b9e57c18cf52e40bde3a602b4b77328d985cd6
conan <conan1160113614@126.com>
2016-06-07 23:58:13 +0800

    1:补充注释

--- app/src/main/java/zhangwenhao/kuafu/com/kuafu_retrofit_exercise/MainActivity.java
+++ app/src/main/java/zhangwenhao/kuafu/com/kuafu_retrofit_exercise/MainActivity.java
@@ -51,6 +51,10 @@ public class MainActivity extends AppCompatActivity {
             case 4:
                 method5();
                 break;
+            case 5:
+                method6();
+                break;
+
         }
 
     }
@@ -201,6 +205,13 @@ public class MainActivity extends AppCompatActivity {
      * subscribeOn ：指定 subscribe（）发生的线程
      * observeOn: 指定 observer 中方法执行的线程
      * <p/>
+     * Schedulers.immediate(): 直接在当前线程运行，相当于不指定线程。这是默认的 Scheduler。
+     * Schedulers.newThread(): 总是启用新线程，并在新线程执行操作。
+     * Schedulers.io(): I/O 操作（读写文件、读写数据库、网络信息交互等）所使用的 Scheduler。行为模式和 newThread() 差不多，区别在于 io() 的内部实现是是用一个无数量上限的线程池，可以重用空闲的线程，因此多数情况下 io() 比 newThread() 更有效率。不要把计算工作放在 io() 中，可以避免创建不必要的线程。
+     * Schedulers.computation(): 计算所使用的 Scheduler。这个计算指的是 CPU 密集型计算，即不会被 I/O 等操作限制性能的操作，例如图形的计算。这个 Scheduler 使用的固定的线程池，大小为 CPU 核数。不要把 I/O 操作放在 computation() 中，否则 I/O 操作的等待时间会浪费 CPU。
+     * 另外， Android 还有一个专用的 AndroidSchedulers.mainThread()，它指定的操作将在 Android 主线程运行
+     * <p/>
+     * <p/>
      * todo ：这里有一个细节 doOnSubscribe 的call方法 是执行在Main线程中的
      * todo ：如果在 doOnSubscribe() 之后有 subscribeOn() 的话，它将执行在离它最近的 subscribeOn() 所指定的线程
      * todo ：如果在 doOnSubscribe() 之后没有subscribeOn 的话，他将执行在当前线程中。
@@ -245,6 +256,14 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+
+    }
+
+
+    /**
+     * flitmap
+     */
+    private void method6() {
 
     }
 }
