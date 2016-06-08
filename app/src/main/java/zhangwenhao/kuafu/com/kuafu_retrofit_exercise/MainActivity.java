ea1b42c65c0c9795ed331c873d215f1425d4df7b
zhang wenhao <zhangwenhao@iboxpay.com>
2016-06-08 15:40:48 +0800

    1:尝试手动

--- app/src/main/java/zhangwenhao/kuafu/com/kuafu_retrofit_exercise/MainActivity.java
+++ app/src/main/java/zhangwenhao/kuafu/com/kuafu_retrofit_exercise/MainActivity.java
@@ -26,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     private static final String TAG = "MainActivity";
+
     private ProgressBar mProgressBar;
+
     private ImageView mImageViewm;
 
     @Override
@@ -35,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
         mImageViewm = (ImageView) findViewById(R.id.image);
-        testFuncation(7);//RxJava基础概念的练习
+        testFuncation(8);//RxJava基础概念的练习
     }
 
     private void testFuncation(int i) {
@@ -64,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case 7:
                 method8();
+                break;
+            case 8:
+                method9();
                 break;
 
         }
@@ -168,7 +173,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private void method4() {
 
-        Observable<String> observable = Observable.just("hello", "rxjava", "auto", "create", "observer");
+        Observable<String> observable = Observable
+                .just("hello", "rxjava", "auto", "create", "observer");
 
         //use action0 no parameter
         Action0 onCompletedAction = new Action0() {
@@ -218,8 +224,10 @@ public class MainActivity extends AppCompatActivity {
      * <p/>
      * Schedulers.immediate(): 直接在当前线程运行，相当于不指定线程。这是默认的 Scheduler。
      * Schedulers.newThread(): 总是启用新线程，并在新线程执行操作。
-     * Schedulers.io(): I/O 操作（读写文件、读写数据库、网络信息交互等）所使用的 Scheduler。行为模式和 newThread() 差不多，区别在于 io() 的内部实现是是用一个无数量上限的线程池，可以重用空闲的线程，因此多数情况下 io() 比 newThread() 更有效率。不要把计算工作放在 io() 中，可以避免创建不必要的线程。
-     * Schedulers.computation(): 计算所使用的 Scheduler。这个计算指的是 CPU 密集型计算，即不会被 I/O 等操作限制性能的操作，例如图形的计算。这个 Scheduler 使用的固定的线程池，大小为 CPU 核数。不要把 I/O 操作放在 computation() 中，否则 I/O 操作的等待时间会浪费 CPU。
+     * Schedulers.io(): I/O 操作（读写文件、读写数据库、网络信息交互等）所使用的 Scheduler。行为模式和 newThread() 差不多，区别在于 io()
+     * 的内部实现是是用一个无数量上限的线程池，可以重用空闲的线程，因此多数情况下 io() 比 newThread() 更有效率。不要把计算工作放在 io() 中，可以避免创建不必要的线程。
+     * Schedulers.computation(): 计算所使用的 Scheduler。这个计算指的是 CPU 密集型计算，即不会被 I/O 等操作限制性能的操作，例如图形的计算。这个
+     * Scheduler 使用的固定的线程池，大小为 CPU 核数。不要把 I/O 操作放在 computation() 中，否则 I/O 操作的等待时间会浪费 CPU。
      * 另外， Android 还有一个专用的 AndroidSchedulers.mainThread()，它指定的操作将在 Android 主线程运行
      * <p/>
      * <p/>
@@ -326,6 +334,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCompleted() {
                 Log.d(TAG, "onCompleted: ");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onNext(Course course) {
+                Log.d(TAG, "onNext: " + course);
+            }
+        });
+    }
+
+
+    /**
+     * 尝试取消注册
+     * 发现在执行完 completed 就已经unsubscribe了
+     * todo 这个的源码要求看下
+     */
+    private void method9() {
+        Observable.from(DataFactory.getData())
+                .flatMap(new Func1<Student, Observable<Course>>() {
+                    @Override
+                    public Observable<Course> call(Student student) {
+                        return Observable.from(student.courses);
+                    }
+                }).subscribe(new Subscriber<Course>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted: ");
+                Log.d(TAG, "unsubscribe " + isUnsubscribed());
+                if (this.isUnsubscribed()) {
+                    unsubscribe();
+                    Log.d(TAG, "unsubscribe ");
+                }
             }
 
             @Override
